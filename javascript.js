d4faf5fbd553be1b0be45ee31e804546837af1e8
MASUD-01 <masudrana96141@gmail.com>
2022-02-17 15:13:41 +0600

    add responsive

--- javascript.js
+++ javascript.js
@@ -1,51 +1,61 @@
+
 document.getElementById('calculate-btn').addEventListener('click', function () {
 
-    // if ( == ' number') {
-    //     alert('give')
-    // }
-    // else {
-    const IncomeValue = document.getElementById('income')
-    const getIncome = IncomeValue.value
-    // console.log(getIncome)
+    const incomeValue = document.getElementById('income')
+    const getIncomeValue = incomeValue.value
 
-    const foodValue = document.getElementById('food').value
-    // console.log(foodValue)
-    const rentValue = document.getElementById('rent').value
+    //check validation, only need number and ignore minus number
+    if (isNaN(getIncomeValue) || getIncomeValue < 0) {
+        alert('please, give only number')
+    }
+    else {
 
-    const clothesValue = document.getElementById('cloth').value
+        //Expenses Value
+        const foodValue = document.getElementById('food').value
+        const rentValue = document.getElementById('rent').value
+        const clothesValue = document.getElementById('cloth').value
 
-    //total epenses calc
-    const totalExpensesValue = parseInt(foodValue) + parseInt(rentValue) + parseInt(clothesValue)
-    document.getElementById('total-expense').innerText = totalExpensesValue
-
-    //calculate balance
-    const totalBalanceCalc = parseInt(getIncome) - totalExpensesValue
-
-    document.getElementById('total-balance').innerText = totalBalanceCalc
-
-    // }
+        //total expenses calculation
+        const totalExpensesValue = parseInt(foodValue) + parseInt(rentValue) + parseInt(clothesValue)
+        const updateTotalExpensesValue = document.getElementById('total-expense')
+        updateTotalExpensesValue.innerText = totalExpensesValue
 
 
+        //calculate balance
+        const totalBalanceCalc = parseInt(getIncomeValue) - totalExpensesValue
+        const totalBalanceUpdate = document.getElementById('total-balance')
+        totalBalanceUpdate.innerText = totalBalanceCalc
+
+        if (getIncomeValue < totalExpensesValue) {
+            alert('your have spend money more than your income')
+        }
+    }
 })
 
 document.getElementById('save-btn').addEventListener('click', function () {
 
-    const IncomeValue = document.getElementById('income')
-    const getIncome = IncomeValue.value
-    //% hisab
+    //calculate saving amount
+    const incomeValue = document.getElementById('income')
+    const getIncomeValue = incomeValue.value
+
     const savingValue = document.getElementById('saving').value
-    const savingPercentage = (getIncome * parseInt(savingValue) / 100)
-    // show saving amount
+    const savingPercentage = (getIncomeValue * parseInt(savingValue) / 100)
+
+
+    // update saving amount
     const savingAmount = document.getElementById('saving-amount')
     savingAmount.innerText = savingPercentage
-
     const totalBalance = document.getElementById('total-balance').innerText
 
-    const remainingBalance = totalBalance - savingAmount.innerText
+    //validation, savingAmount not more than totalBalance
+    if (totalBalance < savingAmount.innerText) {
+        alert("you don't have enough money to saving")
 
-    //show remaining balance
+    }
+
+    //update remaining balance
+    const remainingBalance = totalBalance - savingAmount.innerText
     const remainBalance = document.getElementById('remain-balance')
     remainBalance.innerText = remainingBalance
 
-    console.log(remainingBalance)
 })
